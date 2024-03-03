967d19d9a6f258795ae28fc401c2a989b22a285e
Antonio de Jesus Jaramillo Macias <antoniojm.2133@gmail.com>
2024-03-03 12:16:48 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  navigator.geolocation.getCurrentPosition()
+  
+  return (
+    <div>
+      <h1>Weather App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <img></img>
+      </div>
+      <div>
+        <span>Weather App</span>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
